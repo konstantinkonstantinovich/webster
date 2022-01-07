f87128a096d0298db490a37901a937a43e8b7600
Konstantin <75016737+konstantinkonstantinovich@users.noreply.github.com>
2022-01-07 15:10:18 +0200

    write all model and their  migrations

--- backend/webster-backend/app/Models/User.php
+++ backend/webster-backend/app/Models/User.php
@@ -19,8 +19,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'full_name',
+        'login'
         'email',
+        'avatar'
         'password',
     ];
 
@@ -42,4 +44,29 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
--- backend/webster-backend/database/seeders/DatabaseSeeder.php
+++ backend/webster-backend/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+      DB::table('users')->insert([
+          'login' => Str::random(8),
+          'full_name' => Str::random(10),
+          'email' => Str::random(10).'@gmail.com',
+          'password' => Hash::make('password'),
+      ]);
     }
 }
