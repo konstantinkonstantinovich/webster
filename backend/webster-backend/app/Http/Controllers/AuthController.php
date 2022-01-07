f6aae2e432da7a2c2e69c661d51cd6b62193e995
kvantonio <kvantonio@i.ua>
2022-01-07 16:52:39 +0200

    chenge

--- backend/webster-backend/app/Http/Controllers/AuthController.php
+++ backend/webster-backend/app/Http/Controllers/AuthController.php
@@ -17,7 +17,7 @@ class AuthController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login', 'registration']]);
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
     }
 
     public function login(Request $request) {
@@ -26,24 +26,46 @@ class AuthController extends Controller
             return response()->json(auth()->user(), 200);
         }
         $validator = Validator::make($request->all(), [
-          'login' => 'required|string|min:6',
-          'email' => 'required|email',
-          'password' => 'required|string|min:8',
+            'email' => 'required|email',
+            'password' => 'required|string|min:6'
         ]);
 
         if ($validator->fails()) {
-             return response()->json($validator->errors(), 401);
+            return response()->json($validator->errors(), 422);
         }
 
         if (! $token = auth()->attempt($validator->validated())) {
             $message = [
-               'error' => "Unauthorized",
-               'message' => 'Login or password are incorrect'
+               "error" => 'Unauthorized',
+               "message" => 'Login or password are incorrect'
             ];
             return response()->json($message, 401);
         }
 
         return $this->createNewToken($token);
+    }
+
+    public function register(Request $request) {
+        $validator = Validator::make($request->all(), [
+            "login" => 'required|string',
+            "email" => 'required|string|email|max:100|unique:users',
+            "password" => 'required|string|confirmed|min:6'
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = new User();
+        $user->login = $request->login;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        return response()->json([
+            'message' => 'User successfully registered',
+            'user' => $user
+        ], 201);
     }
 
     protected function createNewToken($token){
