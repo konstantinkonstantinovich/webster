5e04d8ba0c7867dadcf8ed84eec11eddf9bc3432
Viacheslav Kharchenko <vchkhr@gmail.com>
2022-01-10 14:31:16 +0200

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,10 +1,2 @@
-<<<<<<< HEAD
-export const serverURL = ' http://127.0.0.1:3000';
-export const serverSslURL = 'http://127.0.0.1:8000';
-=======
-// export const serverURL = 'http://ucode-webster-fork.herokuapp.com';
-// export const serverSslURL = 'https://ucode-webster-fork.herokuapp.com';
-
 export const serverURL = 'http://localhost:8000';
 export const serverSslURL = 'http://localhost:8000';
->>>>>>> 1d3aa51020e2d1e9e356c8d1ea61cf3dc00ab474
